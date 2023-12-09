0cafe2a2cb86498a634e96be33329124e8cbbbbc
apewo <apewodev@proton.me>
2023-12-09 06:23:32 +0100

    remove comment

--- app/src/applications/calendar/calendar_ui.c
+++ app/src/applications/calendar/calendar_ui.c
@@ -15,13 +15,13 @@ void calendar_ui_show(lv_obj_t *root)
     
     lv_obj_t * calendar = lv_calendar_create(root_page);
     lv_obj_set_size(calendar, 180, 180);
+
     struct tm *time = zsw_clock_get_time();
-    //lv_calendar_set_showed_date(calendar, 2024, 12);
-    //lv_calendar_set_today_date(calendar, 2024, 12,20);
     int year =  time->tm_year + 1900;
     int month = time->tm_mon + 1;
     lv_calendar_set_showed_date(calendar, year, month);
     lv_calendar_set_today_date(calendar, year, month, time->tm_mday);
+
     lv_calendar_header_arrow_create(calendar);
     lv_obj_align(calendar, LV_ALIGN_CENTER, 0, 0);
 }
