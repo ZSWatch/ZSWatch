048805bf6b68b5844249f4904fce1cff810ef764
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2023-12-17 13:39:15 +0100

    Adjust configuration scripts

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,13 +2,12 @@
 # This script must be run from the project root!
 
 JLINK_VERSION=JLink_Linux_V794b_x86_64
+TEMP_DIR="/tmp/downloads"
+CURRENT_DIR=${PWD}
 
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
-TEMP_DIR="tmp"
-
-mkdir ${TEMP_DIR}
 
 echo -e "${GREEN}Enable udev daemon${ENDCOLOR}"
 apt -y install udev
@@ -18,12 +17,12 @@ echo -e "${GREEN}Install J-Link driver${ENDCOLOR}"
 echo -e "${RED}Please note: You automatically accept the Segger license terms when you run this installation!${ENDCOLOR}"
 JLINK_DEB=${TEMP_DIR}/JLink_Linux_x86_64.deb
 curl -s -X POST https://www.segger.com/downloads/jlink/${JLINK_VERSION}.deb H "Content-Type: application/x-www-form-urlencoded" -d "accept_license_agreement=accepted" >$JLINK_DEB
-dpkg -i  $JLINK_DEB
+dpkg -i $JLINK_DEB
 
 echo -e "${GREEN}Initialize project${ENDCOLOR}"
 git config --global --add safe.directory '*'
 git submodule update --init --recursive
-cd app
+cd ${CURRENT_DIR}/app
 west init -l .
 west update
 
@@ -32,5 +31,4 @@ west build --build-dir /workspaces/ZSWatch/app/debug_rev4 /workspaces/ZSWatch/ap
 west build --build-dir /workspaces/ZSWatch/app/release_rev4 /workspaces/ZSWatch/app --pristine --board zswatch_nrf5340_cpuapp@4 --no-sysbuild -- -DNCS_TOOLCHAIN_VERSION=NONE -DBOARD_ROOT=/workspaces/ZSWatch/app;. -DCONF_FILE=/workspaces/ZSWatch/app/prj.conf -DEXTRA_CONF_FILE=/workspaces/ZSWatch/app/boards/release.conf
 
 echo -e "${GREEN}Clean up${ENDCOLOR}"
-
 rm -rf ${PWD}/${TEMP_DIR}
