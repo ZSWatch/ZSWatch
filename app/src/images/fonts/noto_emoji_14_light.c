a57ea1e97cb41d8942834befdf61733c8416481e
LDAB <l.bispo@live.com>
2024-08-06 14:29:34 +0200

    remove fallback from emoji font

--- app/src/images/fonts/noto_emoji_14_light.c
+++ app/src/images/fonts/noto_emoji_14_light.c
@@ -1567,8 +1567,6 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 #endif
 };
 
-extern const lv_font_t lv_font_montserrat_14_full;
-
 /*-----------------
  *  PUBLIC FONT
  *----------------*/
@@ -1594,9 +1592,6 @@ lv_font_t noto_emoji_14_light = {
 #endif
     .dsc = &font_dsc, /*The custom font data. Will be accessed by
                          `get_glyph_bitmap/dsc` */
-#if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
-    .fallback = &lv_font_montserrat_14_full,
-#endif
     .user_data = NULL,
 };
 
